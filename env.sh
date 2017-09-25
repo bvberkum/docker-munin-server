93ef0dd4eb97b427f086992478d76fb49b918a02
B. van Berkum <dev@dotmpe.com>
2017-09-25 03:13:07 +0200

    Build tooling

--- env.sh
+++ env.sh
@@ -1,6 +1,9 @@
-hostname=$(hostname -s)
-domainname=$(hostname)
-DCLR_VOL=/srv/docker-volume-local
-MUNIN_USERS=$(whoami)
-MUNIN_PASSWORDS=$(whoami)
-NODES=boreas.zt
+#!/bin/sh
+{
+  echo hostname=$(hostname -s)
+  echo domainname=$(hostname)
+  echo DCLR_VOL=/srv/docker-volume-local
+  echo MUNIN_USERS=$(whoami)
+  echo MUNIN_PASSWORDS=$(whoami)
+  echo NODES=boreas.zt
+} > .env
